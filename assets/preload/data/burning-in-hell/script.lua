db6acb8442bbafd42afc515263089bafd1c29229
moxie12842 <104624899+moxie12842@users.noreply.github.com>
2022-04-29 08:00:14 -0300

    Add files via upload

---
+++ assets/preload/data/burning-in-hell/script.lua
@@ -0,0 +1,14 @@
+function onStepHit()
+  if curStep==383 then
+    setProperty('bg.visible',false)
+  end
+  if curStep==895 then
+    setProperty('bg.visible',true)
+  end
+  if curStep==1151 then
+    setProperty('bg.visible',false)
+  end
+  if curStep==1408 then
+    setProperty('bg.visible',true)
+    end
+  end
